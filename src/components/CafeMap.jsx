832013f81694111b2d6907eb8bf13568654a6f14
katan4722 <uver7770422@gmail.com>
2021-08-01 21:09:48 +0900

    ♻️ containerStyleのメモ化

--- src/components/CafeMap.jsx
+++ src/components/CafeMap.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
+import { useMemo } from "react";
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
 import { CafeMarker } from "./CafeMarker";
 
-const containerStyle = {
-  width: "500px",
-  height: "240px",
-};
-
 export const CafeMap = ({ center, cafeList, openPlaceId, setOpenPlaceId }) => {
+  const containerStyle = useMemo(() => {
+    return { width: "500px", height: "240px" };
+  }, []);
+
   return (
     <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
